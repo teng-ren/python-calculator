eb1c6314748df9d352dfc5eec0ba77f52c308438
Wee Teng Ren <s10267310@connect.np.edu.sg>
2025-12-04 10:48:12 +0800

    Fix subtract function to return correct difference

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def add(a, b):
 
 def subtract(a, b):
     """Return the difference of two numbers."""
-    return a - a
+    return a - b
 
 def multiply(a, b):
     """Return the product of two numbers."""
